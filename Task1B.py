870d053614b3f480409790cb10281439a0e476dc
Toko Avaliani <Tokoavaliani42@Gmail.com>
2022-01-29 22:19:56 +0000

    Task1B matched Task1B in branch 1B

--- Task1B.py
+++ Task1B.py
@@ -12,8 +12,19 @@ def run():
     stations = build_station_list()
 
     #Calculate distances to stations
-    print(stations_by_distance(stations, (52.2053, 0.1218))[:10])
-    print(stations_by_distance(stations, (52.2053, 0.1218))[-10:])
+    station_distances = stations_by_distance(stations, (52.2053, 0.1218))
+
+    #set up a function which prints station name, station town and distance from the inputted co-ordinates
+    def show_station_distance(station_distances):
+        station_distance_details = []
+        for station_distance in station_distances:
+            station_distance_details.append((station_distance[0].name, station_distance[0].town, station_distance[1]))
+        return station_distance_details
+    
+    #Get station details for the 10 closest stations
+    print(show_station_distance(station_distances[0:10]))
+    #Get station details for the 10 farthest stations
+    print(show_station_distance(station_distances[-10:]))
 
 
 if __name__ == "__main__":
