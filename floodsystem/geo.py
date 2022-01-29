61aebae9e6a4a3bc22fd6635e94c22a014b7299a
Toko Avaliani <Tokoavaliani42@Gmail.com>
2022-01-29 21:42:32 +0000

    Test and Task1B ammend to have correct types

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -12,6 +12,6 @@ from haversine import haversine
 def stations_by_distance(stations, p):
     stations_and_distance = []
     for station in stations:
-        stations_and_distance.append((station.name, haversine(p, station.coord)))
+        stations_and_distance.append((station, haversine(p, station.coord)))
     return sorted_by_key(stations_and_distance, 1)
         
