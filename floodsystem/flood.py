4e27b8c8f49c3b900f283b341feeb13b66d75f7c
Toko Avaliani <Tokoavaliani42@Gmail.com>
2022-02-22 16:27:25 +0000

    Task2C first commit

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -19,3 +19,12 @@ def stations_level_over_threshold(stations, tol=0.8):
         if station.relative_water_level() > tol:
             stations_over_threshold.append((station, station.relative_water_level()))
     return sorted_by_key(stations_over_threshold, 1, reverse=True)
+
+def stations_highest_rel_level(stations, N):
+    """Because the stations_level_over_threshold already orders the stations into a list from highest relative water level to
+    lowest, we can just return the station index (the 0th index) of the first 10 tuples from the over threshold list created in
+    the stations_level_over_threshold function. We are required to return a station object so we return the entire 0th index."""
+    most_risk_stations = []
+    for station in stations_level_over_threshold(stations)[:N]:
+        most_risk_stations.append(station[0])
+    return most_risk_stations
